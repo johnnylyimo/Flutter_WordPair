06dff3f68956f78947591e099c0c77de70825b61
johnnylyimo <johnnylyimo@gmail.com>
2019-12-18 16:36:17 +0300

    Added Scaffold body

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:english_words/english_words.dart';
+import 'package:wordpair_generator/utils/custom_widget.dart';
 
 class HomePage extends StatefulWidget {
   final wordPair = WordPair.random();
@@ -11,20 +12,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'WordPair Generator',
-            style: Theme.of(context).textTheme.title,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'WordPair Generator',
+              style: Theme.of(context).textTheme.title,
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Text(
-          widget.wordPair.asPascalCase,
-          style: Theme.of(context).textTheme.body1,
-        ),
-      ),
-    );
+        body: MyListView());
   }
 }
+
+//Center(
+//child: Text(
+//widget.wordPair.asPascalCase,
+//style: Theme.of(context).textTheme.body1,
+//),
+//),
