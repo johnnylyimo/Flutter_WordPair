d48d8f426ede2f117702d8609538dd252b51dc31
johnnylyimo <johnnylyimo@gmail.com>
2019-12-18 15:25:03 +0300

    Added statefull widget

--- lib/utils/custom_widget.dart
+++ lib/utils/custom_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
 class MyListView extends StatefulWidget {
   @override
   _MyListViewState createState() => _MyListViewState();
